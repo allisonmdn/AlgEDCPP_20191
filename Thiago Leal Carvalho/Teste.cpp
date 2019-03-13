a4468da6e979f48bcea409e462f0b27dae6a2e46
ThiagoLC29 <lptlc@hotmail.com>
2019-03-12 22:11:17 -0300

    Create Teste.cpp

---
+++ Thiago Leal Carvalho/Teste.cpp
@@ -0,0 +1,15 @@
+// Teste.cpp: Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+int main()
+{
+	std::cout << "Obi-Wan Kenobi: Well, hello there" << std::endl;
+	std::cout << "General Grievous: General Kenobi..." << std::endl << std::endl;
+
+	system("pause");
+    return 0;
+}
+
