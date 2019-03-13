58fea7b78054a15c9b4ba4c35b4f9f0e00d66765
BrunoMuniz1 <48338835+BrunoMuniz1@users.noreply.github.com>
2019-03-12 22:13:07 -0300

    Projeto Finalizado - Bruno
    Meu projeto com todo meu cabeçalho

---
+++ Bruno Muniz/Origem.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<fstream>
+#include<ctime>
+#include<cmath>
+using namespace std;
+
+
+void main()
+{
+	setlocale(LC_ALL, "ptb");
+
+	//Bruno Muniz
+	system("pause");
+}
