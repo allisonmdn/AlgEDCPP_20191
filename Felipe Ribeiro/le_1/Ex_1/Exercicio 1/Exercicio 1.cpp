8e23a5267a09b1f2eaa82a2de9ff0076d226ac4d
Felipe Ribeiro <felipe.ribeiro.gamedesign@gmail.com>
2019-03-18 14:48:45 -0300

    Update Exercicio 1.cpp
    Tentando chegar na resolução do exercicio...

--- Felipe Ribeiro/le_1/Ex_1/Exercicio 1/Exercicio 1.cpp
+++ Felipe Ribeiro/le_1/Ex_1/Exercicio 1/Exercicio 1.cpp
@@ -3,19 +3,33 @@
 
 #include "pch.h"
 #include <iostream>
+#include <stdlib.h>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int quantidade;
+	int i;
+
+
+	std::cout << " Por favor informe a quantidade de numeros :" << std::endl;
+	std::cin >> quantidade;
+
+	int *ptrvetor;
+	ptrvetor = (int*)calloc(quantidade,sizeof(int));
+
+	// preenchendo o vetor
+	for ( i = 0; i < quantidade; i++){
+
+		std::cout << " Por favor insira algum numero : " << std::endl;
+		std::cin >> ptrvetor[i];
+		
+	}
+  
+
+	for (i = 0; i < quantidade; i++) {
+		std::cout << ptrvetor[i] << std::endl;
+	}
+	for (i = quantidade; i >= 0; i--) {
+		std::cout << ptrvetor[i] << std::endl;
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
