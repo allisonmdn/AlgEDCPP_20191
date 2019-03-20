62de79302f1584a79e76bf9c850e2a5abda0c5d6
Felipe Ribeiro <felipe.ribeiro.gamedesign@gmail.com>
2019-03-19 21:37:23 -0300

    Update Ex3.cpp
    Expandindo o vetor

--- Felipe Ribeiro/le_1/Ex_3/Ex_3/Ex_3/Ex3.cpp
+++ Felipe Ribeiro/le_1/Ex_3/Ex_3/Ex_3/Ex3.cpp
@@ -1,11 +1,68 @@
 // Ex3.cpp: Define o ponto de entrada para a aplicação de console.
-//
+
+
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <stdlib.h>
 
 int main()
 {
-    return 0;
-}
+	setlocale(LC_ALL, "portuguese");
 
+	int quantidade;
+	int i;
+
+	std::cout << " Por favor informe o tamanho do vetor :" << std::endl;
+	std::cin >> quantidade;
+
+	int *ptrvetor;
+	ptrvetor = (int*)calloc(quantidade, sizeof(int));
+
+	// preenchendo o vetor
+	for (i = 0; i < quantidade; i++) {
+
+		std::cout << " Por favor insira um valor : " << std::endl;
+		std::cin >> ptrvetor[i];
+	}
+	// Devolvendo valores inseridos para o usuário (crescente)
+
+	for (i = 0; i < quantidade; i++) {
+		std::cout << ptrvetor[i] << std::endl;
+	}
+
+	// Expandindo o vetor
+
+	std::cout << " Quantas posições terá o novo vetor ? " << std::endl;
+	std::cin >> quantidade;
+
+	ptrvetor=(int*)realloc(ptrvetor,quantidade* sizeof(int));
+
+	std::cout << "Continue a preencher o vetor, insira o próximo valor ..." << std::endl;
+	std::cin >> quantidade;
+
+
+
+
+
+	// Devolvendo valores inseridos pela o usuário (crescente)
+
+	for (i = 0; i < quantidade; i++) {
+		std::cout << ptrvetor[i] << std::endl;
+	}
+		delete[]ptrvetor;
+
+
+		system("Pause");
+
+	}
+
+
+
+
+
+/*Escrever um programa para alocar dinamicamente um vetor de inteiros,
+preencher este vetor com valores lidos do teclado e mostre na tela os valores do vetor.Após
+solicite ao usuário uma quantidade para realocar expandindo o espaço já alocado, e solicite
+para o mesmo informar novos valores para preencher todo o novo espaço, mantendo os já
+existentes.Ao final libere a memória.Utilize as funções da linguagem C.*/
