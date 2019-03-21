eb0ad9947b04ce65f4bbbbedc0e8d3c3372c35da
CoelhoGB <48338893+CoelhoGB@users.noreply.github.com>
2019-03-21 19:58:52 -0300

    Exercícios

---
+++ Gabriel Coelho/ex1.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//#include <stdlib.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int *vetor;
+//	int auxiliar, x;
+//	cout << "insira quantos numeros" << endl;
+//	cin >> auxiliar;
+//	auxiliar--;
+//	x = auxiliar;
+//	vetor = (int *)malloc(auxiliar * sizeof(float));
+//	cout << "digite os numeros" << endl;
+//	for (; auxiliar >= 0; auxiliar--)
+//	{
+//		cin >> vetor[auxiliar];
+//	}
+//
+//	cout << endl;
+//
+//	for (int auxiliar = x; auxiliar >= 0; auxiliar--)
+//	{
+//		cout << vetor[auxiliar];
+//		cout << endl;
+//	}
+//
+//	free(vetor);
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ Gabriel Coelho/ex2.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int auxiliar, x;
+//	cout << "insira quantos numeros" << endl;
+//	cin >> auxiliar;
+//	auxiliar--;
+//	x = auxiliar;
+//	int * vetor = new int[auxiliar];
+//	cout << "digite os numeros" << endl;
+//	for (; auxiliar >= 0; auxiliar--)
+//	{
+//		cin >> vetor[auxiliar];
+//	}
+//
+//	cout << endl;
+//
+//	for (int auxiliar = x; auxiliar >= 0; auxiliar--)
+//	{
+//		cout << vetor[auxiliar] << endl;
+//	}
+//	delete vetor;
+//	system("pause");
+//	return 0;
