0a789962b2253ef6321e383809d7a5370c881b6a
Giovannecrepaldi <giovannecrepaldi@gmail.com>
2019-03-26 19:49:31 -0300

    Create Lista2Ex1.cpp

---
+++ Giovanne Crepaldi/Lista2Ex1.cpp
@@ -0,0 +1,27 @@
+#include "Guerreiro.h"
+
+
+
+Guerreiro::Guerreiro()
+{
+}
+
+
+Guerreiro::~Guerreiro()
+{
+}
+
+int Guerreiro::vida()
+{
+	return 0;
+}
+
+int Guerreiro::energia()
+{
+	return 0;
+}
+
+int Guerreiro::especial()
+{
+	return 0;
+}
