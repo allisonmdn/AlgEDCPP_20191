ba42a39552d27f0534d520cef2066c31a000818f
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2019-03-26 20:47:17 -0300

    Le2Ex1_aula

---
+++ Raphael/Le2Ex1/Le2Ex1/Guerreiro.cpp
@@ -0,0 +1,62 @@
+#include "Guerreiro.h"
+
+
+
+Guerreiro::Guerreiro()
+{
+	this->hp = 100;
+	this->estamina = 50;
+	this->escudo = false;
+	this->arma = 10;
+	this->armadura = 10;
+}
+
+
+Guerreiro::~Guerreiro()
+{
+}
+
+std::string Guerreiro::treinar()
+{
+	defineEstamina(10);
+	return "Treinando...";
+}
+
+std::string Guerreiro::lutar()
+{
+	defineEstamina(-3);
+	tomarDano();
+	return "lutando";
+}
+
+std::string Guerreiro::defineEstamina(int qtd_estamina)
+{
+	this->estamina += qtd_estamina;
+	if (this->estamina > 50)
+		this->escudo = true;
+	else
+		this->escudo = false;
+	if (qtd_estamina > 0) 
+		return "Aumentou Estamina";
+	else
+		return "Diminuiu Estamina";
+}
+
+std::string Guerreiro::morre()
+{
+	return "morreu";
+}
+
+std::string Guerreiro::tomarDano()
+{
+	if (escudo)
+		this->hp -= 5 / this->armadura;
+	else
+		this->hp -= 10 / this->armadura;
+	if (this->hp <= 0) {
+		morre();
+		return "tomou dano e morreu";
+	}
+	else
+		return "tomou dano";
+}
