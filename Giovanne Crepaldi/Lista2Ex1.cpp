5c578025874435267c527ed0a6525c2945e35165
Giovannecrepaldi <giovannecrepaldi@gmail.com>
2019-03-26 21:30:37 -0300

    teste

--- Giovanne Crepaldi/Lista2Ex1.cpp
+++ Giovanne Crepaldi/Lista2Ex1.cpp
@@ -1,9 +1,20 @@
+#include "..\..\..\..\source\repos\Project54\Project54\Guerreiro.h"
+#include "..\..\..\..\source\repos\Project54\Project54\Guerreiro.h"
+#include "..\..\..\..\source\repos\Project54\Project54\Guerreiro.h"
+#include "..\..\..\..\source\repos\Project54\Project54\Guerreiro.h"
+#include "..\..\..\..\source\repos\Project54\Project54\Guerreiro.h"
+#include "..\..\..\..\source\repos\Project54\Project54\Guerreiro.h"
 #include "Guerreiro.h"
 
 
 
 Guerreiro::Guerreiro()
 {
+	this->hp = 100;
+	this->estamina = 50;
+	this->escudo = false;
+	this->arma = 10;
+	this->armadura = 10;
 }
 
 
@@ -11,17 +22,53 @@ Guerreiro::~Guerreiro()
 {
 }
 
-int Guerreiro::vida()
+void Guerreiro::luta()
 {
-	return 0;
+	defineEstamina(-3);
+	tomaDado();
+	cout << "Lutando" << endl;
 }
 
-int Guerreiro::energia()
+void Guerreiro::treina()
 {
-	return 0;
+	defineEstamina(10);
+	cout << "Treinando..." << endl;
 }
 
-int Guerreiro::especial()
+void Guerreiro::morre()
 {
-	return 0;
+		cout << "MORREU" << endl;
+
+}
+
+
+void Guerreiro::tomaDano()
+{
+	if (escudo)
+		this->hp -= 5 / armadura;
+	else
+		this->hp -= 10 / armadura;
+	
+	if (this->hp <= 0) {
+		morre();
+		cout << "Tomou dano e morreu" << endl;
+	}
+	else
+		cout << "Tomou dano" << endl;
+
+}
+
+void Guerreiro::defineEstamina(int qtd_estamina)
+{
+	this->estamina += qtd_estamina;
+	if (this->estamina > 50)
+		this->escudo = true;
+	else
+		this->escudo = false;
+
+	if (qtd_estamina > 0);
+	cout << "Aumentou Estamina" << endl;
+	else
+
+	cout << "Diminuiu Estamina" << endl;
 }
