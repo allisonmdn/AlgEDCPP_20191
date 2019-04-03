d90ba6d6512896cbf02e24e02976f3d2c6e96b5f
Giovannecrepaldi <giovannecrepaldi@gmail.com>
2019-04-02 22:11:50 -0300

    update

--- Giovanne Crepaldi/fstream.cpp
+++ Giovanne Crepaldi/fstream.cpp
@@ -10,10 +10,33 @@ void main() {
 		cout << "Nâo abriu" << endl;
 	
 	arquivo << 10 << endl << "mapa1" << endl << 2 << 2 << endl;
-	arquivo << "Linha 1 Coluna 1" << "Linha 1 Coluna 2" << endl;
-	arquivo << "Linha 2 Coluna 1" << "Linha 2 Coluna 2" << endl;
+	arquivo << "Linha 1 Coluna 1" << " " << "Linha 1 Coluna 2" << endl;
+	arquivo << "Linha 2 Coluna 1" << " " <<"Linha 2 Coluna 2" << endl;
 
 	arquivo.close();
+
+	ifstream iarquivo;
+	iarquivo.open("teste.txt");
+	if (!iarquivo) {
+		cout << "Não abriu" << endl;
+	}
+
+	int num_mapas;
+	string nome_mapa;
+	iarquivo >> num_mapas;
+	cout << "Numero de mapas = " << num_mapas << endl;
+	iarquivo >> nome_mapa;
+	cout << "Nome do mapa = " << nome_mapa << endl;
+	int num_linhas, num_colunas;
+	iarquivo >> num_linhas >> num_colunas;
+	cout << num_linhas << " " << num_colunas;
+
+	string linha_col1, linha_col2;
+	iarquivo >> linha_col1 >> linha_col2;
+	cout << linha_col1 << " " << linha_col2 << endl;
+
+	iarquivo.close();
+
 
 	system("pause");
 }
