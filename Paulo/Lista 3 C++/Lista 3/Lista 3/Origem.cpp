848ce35299436d8eb39c15641cc17856cb4f96f0
UltraGaren <43190093+UltraGaren@users.noreply.github.com>
2019-04-11 20:35:36 -0300

    Lista 3 C++
    Por enquanto só a 1 tá feita

---
+++ Paulo/Lista 3 C++/Lista 3/Lista 3/Origem.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+
+	//EX1
+	cout << "EX1" << endl;
+	int numRegistros;
+	string nome, data, registros[50];
+	struct pessoas { string nome, data; };
+	pessoas pessoas[50];
+
+	cout << "Digite o numero de registros a seres feitos (max 50, min 0): ";
+	cin >> numRegistros;
+	if (numRegistros > 50 || numRegistros < 1) {
+		cout << "EU DISSE MAXIMO 50 E MINIMO 0 VELHO";
+		return 0;
+	}
+
+	for (int i = 0; i < numRegistros; i++) {
+		cout << "Digite o nome: ";
+		cin >> pessoas[i].nome;
+		cout << "Digite a data: ";
+		cin >> pessoas[i].data;
+	}
+
+	ofstream arqSaida("arqSaida.txt");
+	if (arqSaida.is_open()) {
+		for (int i = 0; i < numRegistros; i++) {
+			arqSaida << pessoas[i].nome << " " << pessoas[i].data << endl;
+		}
+		arqSaida.close();
+	}
+
+	system("pause");
+
+	//EX2
+	cout << "EX2" << endl;
+
+
+	return 0;
+}
