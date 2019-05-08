6a28d6c27f2882902e9bc2989a241b4c4ebc9be0
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2019-05-07 22:12:36 -0300

    Remover

--- Raphael/ListaPessoas/ListaPessoas/Origem.cpp
+++ Raphael/ListaPessoas/ListaPessoas/Origem.cpp
@@ -11,12 +11,23 @@ void main() {
 	Pessoa * p2 = new Pessoa;
 	p2->id = 2;
 
+	Pessoa * p3 = new Pessoa;
+	p3->id = 3;
 	lp_listaPessoas.inserePessoa(p1);
 	lp_listaPessoas.inserePessoa(p2);
+	lp_listaPessoas.inserePessoa(p3);
 
 	std::cout << "Comprimento Lista = " << lp_listaPessoas.contarPessoas()
 		<< std::endl;
 
-	std::cout << "Pessoa Id = " << lp_listaPessoas.buscaPessoaPorID(2)->id; 
+	if (lp_listaPessoas.removePessoa(4))
+		std::cout << "encontrou e removeu" << std::endl;
+	else
+		std::cout << "Não Encontrou" << std::endl;
+
+
+	Pessoa * p = lp_listaPessoas.buscaPessoaPorID(3);
+	if (p != nullptr)
+		std::cout << "Pessoa Id = " << p->id << std::endl; 
 	system("pause");
 }
