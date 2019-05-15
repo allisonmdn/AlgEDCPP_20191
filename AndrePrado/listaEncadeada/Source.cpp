2bc467cc28749c63f34b8019d2f17267a834f0de
André Prado <44608028+AndreLLPrado@users.noreply.github.com>
2019-05-14 21:14:55 -0300

    PegueiExemplosDoSor

---
+++ AndrePrado/listaEncadeada/Source.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "No.h"
+#include "Lista.h"
+
+
+int main() {
+	
+	// Definição de uma pessoa
+	Pessoa * pessoa1;
+	pessoa1 = new Pessoa;
+	pessoa1->s_nome = "Daciolo";
+	pessoa1->i_idade = 40;
+	
+	////adicionar esta pessoa no nó.
+	//n_no = new No;
+	//n_no->pessoa = pessoa1;
+	//n_no->proximo = nullptr;
+	//l_lista->insereFimLista(n_no);
+	////adicionar o nó na lista
+	//n_lista_pessoas = n_no;
+	
+	Pessoa * deux = new Pessoa();
+	deux->s_nome = "DEUX";
+	deux->i_idade = 0;
+
+	std::cout << (pessoa1 == pessoa1) << std::endl;
+	//std::cout << pessoa1 == deux << std::endl;
+	//No * n_no1 = new No;
+	//n_no1->pessoa = DEUX;
+	//n_no1->proximo = nullptr;
+	////adicionar novo nó na lista
+	//n_lista_pessoas->proximo = n_no1;
+
+	//Pessoa * eymael = new Pessoa;
+	//eymael->s_nome = "Eyeyeymael";
+	//eymael->i_idade = 65;
+	//
+	//No * n_no2 = new No;
+	//n_no2->pessoa = eymael;
+	//n_no2->proximo = nullptr;
+
+	//adicionando novo nó na lista
+	//n_lista_pessoas->proximo->proximo = n_no2;
+
+	
+	
+
+	system("pause");
+	return 0;
+}
