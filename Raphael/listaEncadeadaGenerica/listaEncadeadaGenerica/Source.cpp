b7a73cb2b4d6520ce71dd4173a58483173c807a8
Giovannecrepaldi <giovannecrepaldi@gmail.com>
2019-05-16 20:47:11 -0300

    ...

---
+++ Raphael/listaEncadeadaGenerica/listaEncadeadaGenerica/Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Lista.h"
+#include "Pessoa.h"
+
+int main() {
+	
+	No<Pessoa> * no_pessoa = new No<Pessoa>;
+	Lista<Pessoa> * listaPessoas = new Lista<Pessoa>;
+
+	
+
+	// Definição de uma pessoa
+	Pessoa * pessoa1;
+	pessoa1 = new Pessoa;
+	pessoa1->s_nome = "Daciolo";
+	pessoa1->i_idade = 40;
+	
+	listaPessoas->insereFimLista();
+	//lp_lista_pessoas->insereFimLista(pessoa1);
+	
+	Pessoa * deux = new Pessoa();
+	deux->s_nome = "DEUX";
+	deux->i_idade = 0;
+
+	std::cout << (pessoa1 == pessoa1) << std::endl;
+	
+
+
+	system("pause");
+	return 0;
+}
