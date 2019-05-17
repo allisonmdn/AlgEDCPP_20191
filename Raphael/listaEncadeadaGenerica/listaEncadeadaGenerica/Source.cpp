b5314c0fe380517e772bc31267b746b4dce6bab2
Giovannecrepaldi <giovannecrepaldi@gmail.com>
2019-05-16 21:31:29 -0300

    Update Source.cpp

--- Raphael/listaEncadeadaGenerica/listaEncadeadaGenerica/Source.cpp
+++ Raphael/listaEncadeadaGenerica/listaEncadeadaGenerica/Source.cpp
@@ -12,17 +12,28 @@ int main() {
 	// Definição de uma pessoa
 	Pessoa * pessoa1;
 	pessoa1 = new Pessoa;
-	pessoa1->s_nome = "Daciolo";
-	pessoa1->i_idade = 40;
+	pessoa1->s_nome = "Giovanne";
+	pessoa1->i_idade = 18;
+
+	Pessoa * pessoa2;
+	pessoa2 = new Pessoa;
+	pessoa2->s_nome = "Guilherme";
+	pessoa2->i_idade = 20;
 	
-	listaPessoas->insereFimLista();
+	//listaPessoas->insereFimLista();
 	//lp_lista_pessoas->insereFimLista(pessoa1);
 	
-	Pessoa * deux = new Pessoa();
+	/*Pessoa * deux = new Pessoa();
 	deux->s_nome = "DEUX";
-	deux->i_idade = 0;
+	deux->i_idade = 0;*/
+	
+	listaPessoas->insereFimLista(pessoa1);
+	listaPessoas->inserirInicioLista(pessoa2);
 
-	std::cout << (pessoa1 == pessoa1) << std::endl;
+	std::cout << listaPessoas->obtemTamanhoLista;
+
+
+	//std::cout << (pessoa1 == pessoa1) << std::endl;
 	
 
 
