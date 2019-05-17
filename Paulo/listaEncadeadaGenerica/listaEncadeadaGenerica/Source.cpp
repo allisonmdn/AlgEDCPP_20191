4267876b69644227cf433667cc02bd6e39fed7b7
UltraGaren <43190093+UltraGaren@users.noreply.github.com>
2019-05-16 22:24:21 -0300

    lalala
    feito

---
+++ Paulo/listaEncadeadaGenerica/listaEncadeadaGenerica/Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Lista.h"
+#include "Pessoa.h"
+
+int main() {
+	
+	No<Pessoa> * no_pessoa = new No<Pessoa>;
+	Lista<Pessoa> * listaPessoas = new Lista<Pessoa>;
+	
+
+
+	// Definição de uma pessoa
+	Pessoa * pessoa1;
+	pessoa1 = new Pessoa;
+	pessoa1->s_nome = "Daciolo";
+	pessoa1->i_idade = 40;
+	
+	listaPessoas->insereFimLista(pessoa1);
+	
+	Pessoa * deux = new Pessoa();
+	deux->s_nome = "DEUX";
+	deux->i_idade = 0;
+	listaPessoas->insereFimLista(deux);
+	std::cout << (pessoa1 == pessoa1) << std::endl;
+	
+
+
+	system("pause");
+	return 0;
+}
