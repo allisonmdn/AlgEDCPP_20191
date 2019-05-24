c495205d3484cc7403eaec5a4a77b5eced71ef6f
TheoIGorck <48338802+TheoIGorck@users.noreply.github.com>
2019-05-23 21:46:04 -0300

    UpdatePilha

--- Theo/ListaEncadeadGenerica/ListaEncadeadGenerica/Source.cpp
+++ Theo/ListaEncadeadGenerica/ListaEncadeadGenerica/Source.cpp
@@ -2,19 +2,21 @@
 #include "Lista.h"
 #include "Pessoa.h"
 #include "Fila.h"
+#include "Pilha.h"
 
 int main() {
 
 	Fila<Pessoa> * fila_pessoas = new Fila<Pessoa>;
 	No<Pessoa> * no_pessoa = new No<Pessoa>;
 	Lista<Pessoa> * listaPessoas = new Lista<Pessoa>;
-
+	Pilha<Pessoa> * pilhaDePessoas = new Pilha<Pessoa>;
 
 	// Definição de uma pessoa
 	Pessoa * pessoa1;
 	pessoa1 = new Pessoa;
 	pessoa1->s_nome = "Daciolo";
 	pessoa1->i_idade = 40;
+	pilhaDePessoas->empilhar(pessoa1);
 
 	Pessoa * pessoa2;
 	pessoa2 = new Pessoa;
