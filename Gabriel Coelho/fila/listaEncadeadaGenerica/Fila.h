e0c0ec1c8c86941d04e84403b2982592b81cc840
CoelhoGB <coelhogabriel03@gmail.com>
2019-05-23 21:54:28 -0300

    .

---
+++ Gabriel Coelho/fila/listaEncadeadaGenerica/Fila.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+#include "Lista.h"
+template <class T>
+class Fila 
+{
+public:
+	Fila() {};
+	~Fila() {};
+	bool inserirElementoFila(T * elemento); //true - sucesso; false - falha
+	T * removerPrimeiroFila();//Elemento - sucesso; nullptr - falha | RemoveN�Lista
+	T * obterPrimeiroFila(); //Elemento - sucessos; nullptr - falha | N�oRemoveN�Lista
+	T * removerUltimoFila(); //Elemento - sucesso; nullptr - falha | RemoveN�Lista
+	T * obterUltimoFila(); //Elemento - sucesso; nullptr - falha | N�oRemoveN�Lista
+	int obterTamanhoFila();
+	bool esvaziarFila();
+	bool vazia();
+private:
+	Lista<T> * fila = new Lista<T>;
+};
+template <class T>
+bool Fila<T>::inserirElementoFila(T * elemento) {
+	fila->insereFimLista(elemento);
+	return true;
+}
+template <class T>
+T * Fila<T>::removerPrimeiroFila() {
+	T * primElemento = fila->obterPrimeiroElemento();
+	fila->removerPrimeiroNo();
+	return primElemento;
+}
+
+template <class T>
+T * Fila<T>::obterPrimeiroFila() {
+	return fila->obterPrimeiroElemento();
+}
+
+template <class T>
+T * Fila<T>::removerUltimoFila() {
+	T * ultimoElemento = fila->obterUltimoElemento();
+	fila->removerUltimoNo();
+	return ultimoElemento;
+}
+
+template <class T>
+T * Fila<T>::obterUltimoFila() {
+	T * ultimoElemento = 
+	return fila->obterUltimoElemento();
+}
+template <class T>
+int Fila<T>::obterTamanhoFila() {
+	return fila->obtemTamanhoLista();
+}
+template <class T>
+bool Fila<T>::esvaziarfila()
+{
+	return fila->esvaziaLista;
+}
---
+++ Gabriel Coelho/fila/listaEncadeadaGenerica/Pilha.h
@@ -0,0 +1,69 @@
+﻿#pragma once
+#include "Lista.h"
+template <class T>
+class Pilha
+{
+public:
+	Pilha() {};
+	~Pilha() {};
+	bool inserirElementoPilha(T * elemento); //true - sucesso; false - falha
+	void empilhar(T*elemento);
+
+	T * removerPrimeiroPilha();//Elemento - sucesso; nullptr - falha | RemoveN�Lista
+	T * obterPrimeiroPilha(); //Elemento - sucessos; nullptr - falha | N�oRemoveN�Lista
+	T * removerUltimoPilha(); //Elemento - sucesso; nullptr - falha | RemoveN�Lista
+	T * obterUltimoPilha(); //Elemento - sucesso; nullptr - falha | N�oRemoveN�Lista
+	int obterTamanhoPilha();
+	bool esvaziarPilha();
+	bool vazia();
+};
+
+template<class T>
+bool Pilha<T>::inserirElementoPilha(T * elemento)
+{
+	return false;
+}
+
+template<class T>
+inline T * Pilha<T>::removerPrimeiroPilha()
+{
+	return ;
+}
+
+template<class T>
+T * Pilha<T>::obterPrimeiroPilha()
+{
+	return NULL;
+}
+
+template<class T>
+T * Pilha<T>::removerUltimoPilha()
+{
+	return NULL;
+}
+
+template<class T>
+T * Pilha<T>::obterUltimoPilha()
+{
+	return NULL;
+}
+
+template<class T>
+int Pilha<T>::obterTamanhoPilha()
+{
+	return 0;
+}
+
+template<class T>
+bool Pilha<T>::esvaziarPilha()
+{
+	pilha->esvaziaLista();
+	return true;
+}
+
+template<class T>
+bool Pilha<T>::vazia()
+{
+	return fila->listavazia();
+}
+
